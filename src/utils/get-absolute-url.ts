d5bd30e41ebe551f8b9762f826bbcf6637b1ec0c
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-09-22 17:48:54 +0700

    Update get-absolute-url.ts

--- src/utils/get-absolute-url.ts
+++ src/utils/get-absolute-url.ts
@@ -2,6 +2,4 @@ export function getAbsoluteUrl(): string {
   const defaultUrl = 'http://localhost:3000';
 
   if (process.env.APP_URL != null) return process.env.APP_URL;
-
-  return process.env.VERCEL_URL == null ? defaultUrl : `https://${process.env.VERCEL_URL}`;
 }
