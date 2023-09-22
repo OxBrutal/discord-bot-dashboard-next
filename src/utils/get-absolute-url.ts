536269281401c7215cd2458bef700c25538b8425
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-09-22 17:50:49 +0700

    Update get-absolute-url.ts

--- src/utils/get-absolute-url.ts
+++ src/utils/get-absolute-url.ts
@@ -2,4 +2,7 @@ export function getAbsoluteUrl(): string {
   const defaultUrl = 'http://localhost:3000';
 
   if (process.env.APP_URL != null) return process.env.APP_URL;
+
+  return process.env.APP_URL == null ? defaultUrl : `https://web.ikankoi.my
+  id}`;
 }
